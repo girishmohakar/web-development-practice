3097c97b605a1b76021104cca31e9ba806009e3c
girishmohakar <girishmohakarentc@gmail.com>
2025-12-31 17:22:11 +0530

    added javascript level 2 practice set

---
+++ javascript/Level2.js
@@ -0,0 +1,18 @@
+// . At a restaurant you ate: 1 Dal ₹ 100, 2 Roti ₹10
+// each, 1 Ice Cream ₹30, calculate and display the
+// final bill amount.
+// 2. Calculate 18% GST on iPhone15 ₹79,990 and 2
+// Air pods Pro ₹24990 each.
+// 3. Create strings using all 3 methods.
+// 4. Concatenate String with Strings, and String with
+// numbers.
+// 5. Create Order Summary String for our Myntra Cart.
+// 6. Display order summary in a popup
+
+let Ice_cream = 30;
+let Dal = 100;
+let Roti = 10;
+
+let final_bill = (Ice_cream * 1) + (Dal * 1) + (Roti * 2);
+
+console.log(final_bill);
